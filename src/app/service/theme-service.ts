eb4a5929436d4022dbe476a3e6216890359bcc62
Joao Victor <joaovictoralves087@gmail.com>
2026-02-27 11:48:37 -0300

    Adição de dark/light mode

---
+++ src/app/service/theme-service.ts
@@ -0,0 +1,35 @@
+import { Injectable, Renderer2, RendererFactory2} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private isDarkMode:boolean = true;
+  private renderer: Renderer2;
+
+  constructor(rendererFactory: RendererFactory2) {
+    this.renderer = rendererFactory.createRenderer(null, null)
+  }
+
+  getActualTheme(): boolean {
+    return this.isDarkMode;
+  }
+
+  toggleTheme():void{
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      this.renderer.removeClass(document.body, 'light-theme');
+      localStorage.setItem('theme', 'light');
+    } else {
+      this.renderer.addClass(document.body, 'light-theme');
+      localStorage.setItem('theme', 'dark');
+    }
+  }
+  initTheme() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+      this.isDarkMode = true;
+      this.renderer.addClass(document.body, 'dark-theme');
+    }
+  }
+}
