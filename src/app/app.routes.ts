e16af3370affa4d91d10f7716a77630748e9eb62
Joao Victor <joaovictoralves087@gmail.com>
2026-02-28 13:57:39 -0300

    Primeira ideia do professional page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import {Contact} from './pages/contact/contact';
 import {Professional} from './pages/professional/professional';
-import {Resume} from './pages/resume/resume';
 import {Home} from './pages/home/home';
 
 export const routes: Routes = [
   {path: "", component: Home},
   {path: "contact", component: Contact},
   {path: "professional", component: Professional},
-  {path: "resume", component: Resume},
   {path: "**", redirectTo: ""}
 ];
