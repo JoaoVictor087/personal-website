5a4f51673d51f81da7b78c18fa3bbcdff1e434a7
Joao Victor <joaovictoralves087@gmail.com>
2026-02-28 12:41:29 -0300

    Seção footer criada

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import {ThemeService} from '../../service/theme-service';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './footer.html'
 })
 export class Footer {
+  constructor(public themeservice: ThemeService) {
+  }
+
+  protected getTheme():boolean{
+    return this.themeservice.getCurrentTheme();
+  }
 
 }
